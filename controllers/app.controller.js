2dae2c54cb096238c875b516ec14c3b01115b81e
Raunak Mandal <iamriju2000@gmail.com>
2022-09-28 20:55:56 +0530

    Move to Axios

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -1,5 +1,5 @@
 const { pool, TABLE_NAME } = require("../conn");
-const fetch = require("node-fetch");
+const axios = require("axios");
 
 exports.showMethods = (req, res) => {
   return res.status(200).json({
@@ -91,48 +91,43 @@ exports.showLimit = async (req, res) => {
 };
 exports.fetchAndSend = async (req, res) => {
   // console.log("Fetching price...");
-  let val = await fetch("https://api.coingecko.com/api/v3/exchange_rates")
+  let val = await axios
+    .get("https://api.coingecko.com/api/v3/exchange_rates")
     .then((result) => {
       return result;
     })
     .catch((err) => {
       return err;
     });
-
   if (val.status === 200) {
-    val
-      .json()
-      .then((result) => {
-        const data = {
-          _id: "INR" + Math.floor(Math.random() * 899999 + 100000),
-          update_time: new Date().toString(),
-          inr_value: result.rates.inr.value,
-        };
-        pool.query(
-          `INSERT INTO ${TABLE_NAME} (_id, update_time, inr_value) VALUES($1, $2, $3)`,
-          [data._id, data.update_time, data.inr_value],
-          (err) => {
-            if (err) {
-              return res.status(500).json({
-                status: "Failure",
-                code: 500,
-                error: err,
-              });
-            }
-            return res.status(200).json({
-              status: "Success",
-              code: 200,
-              data: data,
-            });
-          }
-        );
-      })
-      .catch((err) => {
-        return res.status(400).json({
-          status: "Error",
-          code: 400,
-          error: err,
+    const data = {
+      _id: "INR" + Math.floor(Math.random() * 899999 + 100000),
+      update_time: new Date().toString(),
+      inr_value: val.data.rates.inr.value,
+    };
+    pool.query(
+      `INSERT INTO ${TABLE_NAME} (_id, update_time, inr_value) VALUES($1, $2, $3)`,
+      [data._id, data.update_time, data.inr_value],
+      (err) => {
+        if (err) {
+          return res.status(500).json({
+            status: "Failure",
+            code: 500,
+            error: err,
+          });
+        }
+        return res.status(200).json({
+          status: "Success",
+          code: 200,
+          data: data,
         });
-      });
+      }
+    );
+  } else {
+    return res.status(500).json({
+      status: "Failure",
+      code: 500,
+      error: "Error fetching data",
+    });
   }
 };
--- controllers/cron.controller.js
+++ controllers/cron.controller.js
@@ -1,13 +1,14 @@
 const { pool, TABLE_NAME } = require("../conn");
 var CronJob = require("cron").CronJob;
-const fetch = require("node-fetch");
+const axios = require("axios");
 exports.job = new CronJob("*/30 * * * *", () => {
   console.log("Fetching price every 30 minute...");
   apiCall();
 });
 
 const apiCall = async () => {
-  let val = await fetch("https://api.coingecko.com/api/v3/exchange_rates")
+  let val = await axios
+    .get("https://api.coingecko.com/api/v3/exchange_rates")
     .then((result) => {
       return result;
     })
@@ -15,26 +16,34 @@ const apiCall = async () => {
       return err;
     });
   if (val.status === 200) {
-    val
-      .json()
-      .then((result) => {
-        const data = {
-          _id: "INR" + Math.floor(Math.random() * 899999 + 100000),
-          update_time: new Date().toString(),
-          inr_value: result.rates.inr.value,
-        };
-        pool.query(
-          `INSERT INTO ${TABLE_NAME} (_id, update_time, inr_value) VALUES($1, $2, $3)`,
-          [data._id, data.update_time, data.inr_value],
-          (err) => {
-            if (err) {
-              console.log("Error: " + err);
-            }
-          }
-        );
-      })
-      .catch((err) => {
-        console.log("Error: " + err);
-      });
+    const data = {
+      _id: "INR" + Math.floor(Math.random() * 899999 + 100000),
+      update_time: new Date().toString(),
+      inr_value: val.data.rates.inr.value,
+    };
+    pool.query(
+      `INSERT INTO ${TABLE_NAME} (_id, update_time, inr_value) VALUES($1, $2, $3)`,
+      [data._id, data.update_time, data.inr_value],
+      (err) => {
+        if (err) {
+          return res.status(500).json({
+            status: "Failure",
+            code: 500,
+            error: err,
+          });
+        }
+        return res.status(200).json({
+          status: "Success",
+          code: 200,
+          data: data,
+        });
+      }
+    );
+  } else {
+    return res.status(500).json({
+      status: "Failure",
+      code: 500,
+      error: "Error fetching data",
+    });
   }
 };
