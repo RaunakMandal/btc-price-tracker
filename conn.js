99c16da85c4f94f48d6559c10afe8ec632a0d1ee
Raunak Mandal <iamriju2000@gmail.com>
2022-09-28 23:05:53 +0530

    fix postgres table

--- conn.js
+++ conn.js
@@ -7,3 +7,15 @@ exports.pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
+
+this.pool.query(
+  "CREATE TABLE btctoinr(_id text PRIMARY KEY, inr_value text,update_time text);",
+  (err, res) => {
+    if (err) {
+      console.log("Table already exists");
+    } else {
+      console.log("Table created");
+    }
+    this.pool.end();
+  }
+);
